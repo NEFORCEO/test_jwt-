87d9b78eb77c00b53623f3c0373a9dae53bffd14
= <=>
2025-12-14 13:04:50 +0700

    update

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+import os 
+from dotenv import load_dotenv
+
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
+load_dotenv()
 
 class Config(BaseModel):
-    private_key: str = "my_app_key"
-    algoritm: str = "HS256"
+    private_key: str = os.getenv("PRIVATE_KEY")
+    algoritm: str = os.getenv("ALGORITM")
 
 
 class Settings(BaseSettings):
@@ -12,4 +16,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
